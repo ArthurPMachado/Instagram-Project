380fb61316ae78b39085a415a235987aa7a7ebf4
Arthur Pereira Machado <machadoparthur1@gmail.com>
2019-06-13 00:57:31 -0300

    Acessing mongoDB atlas and creating routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,15 @@
 //const variables for application with express
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.send(`Hello ${req.query.name}`);
-})
+//connecting to mongoDB atlas
+mongoose.connect('mongodb+srv://Arthur:a2r4t6h8u0r@cluster0-ddy7h.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+});
+
+app.use(require('./routes'));
 
 app.listen(3000);
+
---
+++ backend/src/routes.js
@@ -0,0 +1,9 @@
+//const variables
+const express = require('express');
+const routes = new express.Router();
+
+routes.get('/', (req, res) => {
+    return res.send(`Hello ${req.query.name}`);
+})
+
+module.exports = routes;
