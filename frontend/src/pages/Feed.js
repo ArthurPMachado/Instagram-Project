cf01c30586210150c16f51c8d51de14e2cf6cc01
Arthur Pereira Machado <machadoparthur1@gmail.com>
2019-06-14 17:50:56 -0300

    Like button working

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -20,6 +20,10 @@ class Feed extends Component {
         this.setState({ feed: response.data });
     }
 
+    handleLike = id => {
+        api.post(`/posts/${id}/like`);
+    }
+
     render() {
         return (
             <section id="post-list">
@@ -38,7 +42,9 @@ class Feed extends Component {
 
                         <footer>
                             <div className="actions">
-                                <img src={like} alt="" />
+                                <button type="button" onClick={() => this.handleLike(post._id)}>
+                                    <img src={like} alt="" />
+                                </button>
                                 <img src={comment} alt="" />
                                 <img src={send} alt="" />
                             </div>
