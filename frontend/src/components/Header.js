94e0d63356954b0e2cd70afbd068841638a8193c
Arthur Pereira Machado <machadoparthur1@gmail.com>
2019-06-14 12:18:32 -0300

    Minor changes in header layout, using link

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './Header.css';
+
+import logo from '../assets/logo.svg';
+import camera from '../assets/camera.svg';
+
 
 // import { Container } from './styles';
 
 export default function Header() {
   return (
     <header id="main-header">
-        <div className="header-content">
-            <img src="" alt="InstaRocket"/>
-            <img src="" alt="Enviar publicação"/>
-        </div>
+      <div className="header-content">
+        <Link to="/">
+          <img src={logo} alt="InstaRocket" />
+        </Link>
+        <Link to="/new">
+          <img src={camera} alt="Enviar publicação" />
+        </Link>
+      </div>
     </header>
   );
 }
