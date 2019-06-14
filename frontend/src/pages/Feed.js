a046fb3cfc71e807b62811298086c470abfc5c44
Arthur Pereira Machado <machadoparthur1@gmail.com>
2019-06-14 12:50:17 -0300

    Feed design almost finished

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,9 +1,74 @@
 import React, { Component } from 'react';
 
+import './Feed.css';
+
+import more from '../assets/more.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
+import send from '../assets/send.svg';
+
+
 class Feed extends Component {
     render() {
-        return(
-            <h1>Feed</h1>
+        return (
+            <section id="post-list">
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Arthur Pereira Machado</span>
+                            <span className="place">São Paulo</span>
+                        </div>
+
+                        <img src={more} alt="Mais" />
+                    </header>
+
+                    <img src="http://localhost:3333/files/Dark Souls.jpg" alt="" />
+
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt="" />
+                            <img src={comment} alt="" />
+                            <img src={send} alt="" />
+                        </div>
+
+                        <strong>1000 curtidas</strong>
+
+                        <p>
+                            Um post feito durante o bootcamp
+                            <span>#react #bootcamp #top</span>
+                        </p>
+                    </footer>
+                </article>
+
+                
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Arthur Pereira Machado</span>
+                            <span className="place">São Paulo</span>
+                        </div>
+
+                        <img src={more} alt="Mais" />
+                    </header>
+
+                    <img src="http://localhost:3333/files/Dark Souls.jpg" alt="" />
+
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt="" />
+                            <img src={comment} alt="" />
+                            <img src={send} alt="" />
+                        </div>
+
+                        <strong>1000 curtidas</strong>
+
+                        <p>
+                            Um post feito durante o bootcamp
+                            <span>#react #bootcamp #top</span>
+                        </p>
+                    </footer>
+                </article>
+            </section>
         );
     }
 }
